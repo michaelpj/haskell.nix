b6ac8605c53203c41c6a6a6e83a05c06278e444b
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2019-05-20 19:33:14 +1000

    wip: shellFor depends of depends

--- builder/shell-for.nix
+++ builder/shell-for.nix
@@ -1,17 +1,33 @@
-{ lib, stdenv, glibcLocales, ghcForComponent, makeConfigFiles, hsPkgs }:
+{ lib, stdenv, glibcLocales, pkgconfig, ghcForComponent, makeConfigFiles, hsPkgs }:
 
 { packages, withHoogle ? true, ... } @ args:
 
 let
   selected = packages hsPkgs;
+  selectedConfigs = map (p: p.components.all.config) selected;
+
   name = if lib.length selected == 1
     then "ghc-shell-for-${(lib.head selected).name}"
     else "ghc-shell-for-packages";
+
+  # If `packages = [ a b ]` and `a` depends on `b`, don't build `b`,
+  # because cabal will end up ignoring that built version, assuming
+  # new-style commands.
+  packageInputs = lib.filter
+    (input: lib.all (cfg: input.identifier != cfg.identifier) selected)
+    (lib.concatMap (cfg: cfg.depends) selectedConfigs);
+
+  # fixme: messy duplication from comp-builder.nix
+  nativeBuildInputs = lib.concatMap (cfg: (lib.concatMap (c: if c.isHaskell or false
+      then builtins.attrValues (c.components.exes or {})
+      else [c]) cfg.build-tools) ++ lib.optional (cfg.pkgconfig != []) pkgconfig) selectedConfigs;
+  systemInputs = lib.concatMap (cfg: cfg.libs ++ cfg.frameworks ++ lib.concatLists cfg.pkgconfig) selectedConfigs;
+
   configFiles = makeConfigFiles {
     fullName = args.name or name;
     identifier.name = name;
     component = {
-      depends = selected;
+      depends = packageInputs;
       libs = [];
       frameworks = [];
       doExactConfig = false;
@@ -22,10 +38,6 @@ let
     inherit configFiles;
   };
   mkDrvArgs = builtins.removeAttrs args ["packages" "withHoogle"];
-
-  # fixme: check if systemInputs and nativeBuildInputs are necessary
-  systemInputs = [];
-  nativeBuildInputs = [];
 in
   stdenv.mkDerivation (mkDrvArgs // {
     name = mkDrvArgs.name or name;
